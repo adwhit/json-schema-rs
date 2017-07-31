f242d548d2c43c219c22e6f6fc5b92fceeaccb6a
Alex Whitney <adwhit@fastmail.com>
2017-07-31 22:03:55 +0100

    move schema into own module

---
+++ src/schema.rs
@@ -0,0 +1,140 @@
+use serde_json;
+use std::collections::BTreeMap;
+pub type Map<T> = BTreeMap<String, T>;
+
+pub type PositiveInteger = i64;
+pub type PositiveIntegerDefault0 = serde_json::Value;
+pub type SchemaArray = Vec<Schema>;
+pub type StringArray = Vec<String>;
+
+#[derive(Clone, PartialEq, Debug, Default, Deserialize, Serialize)]
+pub struct Schema {
+    #[serde(rename = "$ref")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub ref_: Option<String>,
+    #[serde(rename = "$schema")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub schema: Option<String>,
+    #[serde(rename = "additionalItems")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub additional_items: Option<serde_json::Value>,
+    #[serde(rename = "additionalProperties")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub additional_properties: Option<BoolOrSchema>,
+    #[serde(rename = "allOf")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub all_of: Option<SchemaArray>,
+    #[serde(rename = "anyOf")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub any_of: Option<SchemaArray>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub default: Option<serde_json::Value>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub definitions: Option<Map<Schema>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub dependencies: Option<Map<serde_json::Value>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub description: Option<String>,
+    #[serde(rename = "enum")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub enum_: Option<Vec<serde_json::Value>>,
+    #[serde(rename = "exclusiveMaximum")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub exclusive_maximum: Option<bool>,
+    #[serde(rename = "exclusiveMinimum")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub exclusive_minimum: Option<bool>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub id: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub items: Option<SchemaOrSchemas>,
+    #[serde(rename = "maxItems")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub max_items: Option<PositiveInteger>,
+    #[serde(rename = "maxLength")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub max_length: Option<PositiveInteger>,
+    #[serde(rename = "maxProperties")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub max_properties: Option<PositiveInteger>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub maximum: Option<f64>,
+    #[serde(rename = "minItems")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub min_items: Option<PositiveIntegerDefault0>,
+    #[serde(rename = "minLength")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub min_length: Option<PositiveIntegerDefault0>,
+    #[serde(rename = "minProperties")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub min_properties: Option<PositiveIntegerDefault0>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub minimum: Option<f64>,
+    #[serde(rename = "multipleOf")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub multiple_of: Option<f64>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub not: Option<Box<Schema>>,
+    #[serde(rename = "oneOf")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub one_of: Option<SchemaArray>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub pattern: Option<String>,
+    #[serde(rename = "patternProperties")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub pattern_properties: Option<Map<Schema>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub properties: Option<Map<Schema>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub required: Option<StringArray>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub title: Option<String>,
+    #[serde(rename = "type")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub type_: Option<SimpleTypeOrSimpleTypes>,
+    #[serde(rename = "uniqueItems")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub unique_items: Option<bool>,
+}
+
+#[serde(rename = "simpleTypes")]
+#[derive(Clone, Copy, PartialEq, Eq, Debug, Deserialize, Serialize, Hash)]
+pub enum SimpleType {
+    #[serde(rename = "array")]
+    Array,
+    #[serde(rename = "boolean")]
+    Boolean,
+    #[serde(rename = "integer")]
+    Integer,
+    #[serde(rename = "null")]
+    Null,
+    #[serde(rename = "number")]
+    Number,
+    #[serde(rename = "object")]
+    Object,
+    #[serde(rename = "string")]
+    String,
+}
+
+#[derive(Clone, PartialEq, Debug, Deserialize, Serialize)]
+#[serde(untagged)]
+pub enum BoolOrSchema {
+    Bool(bool),
+    Schema(Box<Schema>),
+}
+
+#[derive(Clone, PartialEq, Debug, Deserialize, Serialize)]
+#[serde(untagged)]
+pub enum SimpleTypeOrSimpleTypes {
+    SimpleType(SimpleType),
+    SimpleTypes(Vec<SimpleType>),
+}
+
+#[derive(Clone, PartialEq, Debug, Deserialize, Serialize)]
+#[serde(untagged)]
+pub enum SchemaOrSchemas {
+    Schema(Box<Schema>),
+    Schemas(Schemas),
+}
+
+type Schemas = Vec<Schema>;
